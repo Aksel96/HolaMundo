31ff55dbf1f118fc908e91c80f8466cbf637a79d
Aksel <jonhaksel@gmail.com>
2023-07-31 16:04:20 -0600

    ejercicio 1 de entrada y salida

---
+++ src/main/kotlin/ejercicio1InputOutput.kt
@@ -0,0 +1,15 @@
+fun main(){
+    /*Ejercicio_ Preguntar al usuario algunos datos importantes
+    para crear su cuenta de usuario (Usuario, Contraseña, Correo)
+    y luego mostrar dicha informacion
+     */
+
+    println("---Crear Cuenta---")
+    println("Introduce tu usuario:")
+    var usuario = readLine()
+    println("Introduce tu Contraseña:")
+    var contrasenia = readLine()
+    println("Introduce tu Correo:")
+    var correo = readLine()
+    println("Tu Usuario es: $usuario, tu contraseña es $contrasenia y tu correo es $correo")
+}
