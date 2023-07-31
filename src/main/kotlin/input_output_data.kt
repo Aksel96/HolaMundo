de08d6da6b0a7ae8e4d3a2e277fa8c46b2fd77bb
Aksel <jonhaksel@gmail.com>
2023-07-31 15:46:48 -0600

    input y output en Kotlin

---
+++ src/main/kotlin/input_output_data.kt
@@ -0,0 +1,13 @@
+fun main(){
+    // Ingreso de datos desde la consola
+    //println("Hola Mundo!")
+    //println("Linea 2")
+    //readLine() //Espera a un input del usuario
+
+    //Ejemplo
+
+    println("¿Cual es tu color favorito?")
+    val color = readLine()
+    println("Tu color es : $color")
+
+}
